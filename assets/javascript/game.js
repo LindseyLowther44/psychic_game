fb779602aac26de7d0f75297f4093879fbbcc89d
LindseyLowther44 <lindseymlowther@gmail.com>
2020-02-14 21:22:42 -0600

    added files and folders

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+const choices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+var wins = 0;
+var loss = 0;
+var lettersUsed= [];
+var guessesleft = ;
+var maxGuess =
