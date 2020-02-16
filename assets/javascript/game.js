9394cf51e25f311e1d65af1d09a7149f4dd34ce5
LindseyLowther44 <lindseymlowther@gmail.com>
2020-02-15 18:49:23 -0600

    added css and javascript functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,46 @@
 const choices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-let wins = 0;
-let loss = 0;
+let wins = 1;
+let loss = 1;
 let lettersUsed= [];
-let guessRemaining = 7;
+let guessRemaining = 10;
 let computerGuess;
 let letterIndex;
+
 
 document.onkeyup = function (event){
     var playerGuess = event.key;
     
 
     if (playerGuess === computerGuess) {
-        wins++;
-        document.getElementsByClassName('wins').innerHTML = wins;    
+        document.getElementById('wins').innerHTML = "<p>Wins:" + wins++ + "</p>";    
         alert("You win!")
         reset()
     } else {
         guessRemaining--;
         console.log("next guess!");
-        document.getElementsByClassName('guessesLeft').innerHTML = loss;
-        document.getElementsByClassName('guess-Sofar').innerHTML = lettersUsed;
         console.log(lettersUsed);
+        document.getElementById("remainingGuesses").innerHTML = "<p>Guesses Remaining: " +guessRemaining + "</p>";
+        lettersUsed.push(playerGuess);
+        document.getElementById("lettersUsed").innerHTML = "<p>Letters Guessed:" + lettersUsed.join(', ') + "</p>";
+        playerGuess.innerHTML = lettersUsed.join(',');
     }
-    if (guessRemaining <= 0) {
-        loss++;
-        document.getElementsByClassName()
-        guessRemaining = 7;
+    if (guessRemaining <= 0){
+        document.getElementById("losses").innerHTML ="<p>Loss:" + loss++ + "</p>";
+        guessRemaining = 10;
         lettersUsed = [];
+        reset()
     }
+    
 }
 
+
 function reset () {
-    guessRemaining = 7;
+    guessRemaining = 10;
     lettersUsed =[]; 
     computerGuess = choices[letterIndex];
     letterIndex = Math.floor(Math.random() * choices.length)
     computerGuess = choices[letterIndex];
-    console.log(computerGuess)
 }
 
 reset()
